2330c6df383a32ae63c9d3b9f922277726a30da2
Dumydevel <kajjo282@nemi.tskoli.is>
2017-12-01 13:16:01 +0000

    Update Soldiers.py

--- Soldiers.py
+++ Soldiers.py
@@ -1,39 +1,13 @@
-from random import *
+rom random import *
+
+
+
+
 
 class Soldiers:
 # Býr til vopn fyrir hermenina
     def weaponstats(self):
-        if self.weapon == 1:
-            return "Vibro-blade"
-        elif self.weapon == 2:
-            return "Electrostaff"
-        elif self.weapon == 3:
-            return "Blaster"
-        elif self.weapon == 4:
-            return "Sniper"
-        elif self.weapon == 5:
-            return "Lightsaber"
-        elif self.weapon == 6:
-            return "Heavy Blaster pistol"
-        elif self.weapon == 7:
-            return "Bowcaster"
-        elif self.weapon == 8:
-            return "Wrist rockets"
-        elif self.weapon == 9:
-            return "Disruptor"
-        elif self.weapon == 10:
-            return "Force pike"
-        elif self.weapon == 11:
-            return "Vibro-az"
-        elif self.weapon == 12:
-            return "Ion Blaster"
-        elif self.weapon == 13:
-            return "Minigun"
-        elif self.weapon == 14:
-            return "Thermal detonator"
-        else:
-            return "BOII THAT AINT A WEAPON THAT WE HAVE!"
-
+        return ["Vibro-blade","Electrostaff","Blaster","Sniper","Lightsaber","Heavy Blaster pistol","Bowcaster","Wrist rockets","Disruptor","Force pike","Vibro-az","Ion Blaster","Minigun","Thermal detonator"][self.weapon-1]
         return "Unknown"
     #setur líf, kraft og vopn fyrir hermenina
     def __init__(self, name, live=0, weapon=1, power=0):
